b0f6967b535479e965a8ca6ea5ff6c6d7bc0b569
Galiant <antonijodev@gmail.com>
2018-10-31 21:27:45 +0000

    Added post method for add book

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 var Book = require('../models/book');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('shop/index', { title: 'The Book Boutique' });
 });
 
 /* GET add book */
-router.get('/manage/add', function(req, res) {
+router.get('/manage/add', (req, res) => {
   res.render('manage/add');
+});
+
+/* POST add book */
+router.post('/shop/all', (req, res) => {
+  res.send('ok');
 });
 
 module.exports = router;
