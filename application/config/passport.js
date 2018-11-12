93c82a2c82904987f7cc36aee333823420e5de31
Galiant <antonijodev@gmail.com>
2018-11-12 22:11:13 +0000

    Return message if no password  in database during login session

--- application/config/passport.js
+++ application/config/passport.js
@@ -11,12 +11,24 @@ module.exports = (passport) => {
       usernameField: 'email'
     },
     (email, password, done) => {
+      // match user
       User.findOne({
         email: email
       }).then(user => {
         if (!user) {
-          return done(null, false, { message: 'No user found' });
+          return done(null, false, { message: 'No user found with this email address!' });
         }
+
+        // match password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          }
+          else {
+            return done(null, false, { message: 'Password incorrect. Please try again!' });
+          }
+        })
       })
     }));
 };
